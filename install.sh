033642d7183f38dc9335772ae84f93eb4dea487b
G_cat <gcatmail2@gmail.com>
2021-12-26 16:00:59 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 /usr/bin/python3 -m pip install -r ./requirements.txt # Installing requirements from requirements.txt
 cd ./src/wfetch # Cd-ing into src/wfetch directory
 mv ./wfetch.py ./wfetch # Removing .py from the wfetch.py name
-cp -a ./src/wfetch/. /usr/local/bin # Copying all of the files to path
+cp -a ./. /usr/local/bin # Copying all of the files to path
 cd ../../../ # Un-cding
 rm -rf ./wfetch # Deleting the copied github folder
