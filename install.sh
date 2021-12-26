c07065ef231c41892e5e11a462d0b95447dc0919
G_cat <gcatmail2@gmail.com>
2021-12-26 16:06:52 +0300

    Removed src directory

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 # INSTALL SCRIPT
 
 /usr/bin/python3 -m pip install -r ./requirements.txt # Installing requirements from requirements.txt
-cd ./src/wfetch # Cd-ing into src/wfetch directory
+cd ./wfetch # Cd-ing into src/wfetch directory
 mv ./wfetch.py ./wfetch # Removing .py from the wfetch.py name
 cp -a ./. /usr/local/bin # Copying all of the files to path
-cd ../../../ # Un-cding
+cd ../../ # Un-cding
 rm -rf ./wfetch # Deleting the copied github folder
